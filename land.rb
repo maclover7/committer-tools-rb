33e595eba0b1d5271ab3ad41edf2902d6fd4997f
Jon Moss <me@jonathanmoss.me>
2017-11-05 17:00:05 -0500

    Get PR ID

--- land.rb
+++ land.rb
@@ -1,5 +1,12 @@
 class Lander
+  def initialize
+    @pr = {}
+  end
+
   def run
+    @pr = get_pr();
+    puts @pr
+
     # get pr id
     # get pr from github api
     # put together metadata for commit
@@ -7,8 +14,18 @@ class Lander
     # figure out squashing commit, make sure final message ok
     # push commit
   end
+
+  private
+
+  def get_pr
+    puts "Please enter PR ID:"
+    pr_id = gets.strip!
+
+    org, repo_and_id = pr_id.split('/')
+    repo, id = repo_and_id.split('#')
+
+    { org: org, repo: repo, id: id }
+  end
 end
 
 Lander.new.run
-
-puts 'hi!'
