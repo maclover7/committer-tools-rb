7bb69d5b342228419e53f0e8a243668a2642d3ce
Jon Moss <me@jonathanmoss.me>
2017-11-05 16:57:56 -0500

    add notes about basic structure

--- land.rb
+++ land.rb
@@ -1,1 +1,14 @@
+class Lander
+  def run
+    # get pr id
+    # get pr from github api
+    # put together metadata for commit
+    # do a few checks, make sure ok to land
+    # figure out squashing commit, make sure final message ok
+    # push commit
+  end
+end
+
+Lander.new.run
+
 puts 'hi!'
